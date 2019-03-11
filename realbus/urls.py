976bc6d945787f66d9cf482426d991db79a60738
chanzz <chanhaoji@qq.com>
2019-03-11 22:21:35 +0800

    fix problem

--- realbus/urls.py
+++ realbus/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('search/', include('search_bus.urls',namespace='search_bus')),
+    path('', include('search_bus.urls',namespace='search_bus')),
 ]
